d3dd4b60edc600cd8fa64debfa6b54caeb0e1b69
chiazo <chiazo@live.unc.edu>
2025-06-08 11:21:40 -0400

    note positioning on fretboard

--- src/code/fretboard.js
+++ src/code/fretboard.js
@@ -1,11 +1,12 @@
 import { Scale, Type } from "./scale.js";
 import { Note, Sign, Direction } from "./note.js";
-import { Chord } from "./chord.js";
+import { Chord, allChords } from "./chord.js";
 
 export class Fretboard {
   constructor() {
     this.capo = 0;
     this.chords = [];
+    this.openStringNotes = this.resetOpenStrings();
   }
 
   getChords() {
@@ -21,7 +22,9 @@ export class Fretboard {
     }
 
     if (position > this.capo) {
-      this.transposeChords(position, Direction.UP);
+      this.transposeChords(position);
+      this.transposeNotes(position);
+      this.capo = position;
     } else {
       this.removeCapo();
       this.addCapo(position);
@@ -30,15 +33,25 @@ export class Fretboard {
 
   transposeChords(newPosition = 1) {
     var intervals = Math.abs(this.capo - newPosition);
-    while (intervals >= 0) {
+    while (intervals > 0) {
       this.chords.map((c) => c.transpose());
       intervals--;
     }
-    this.capo = newPosition;
+  }
+
+  transposeNotes(newPosition = 1) {
+    var intervals = Math.abs(this.capo - newPosition);
+    while (intervals > 0) {
+      Object.entries(this.openStringNotes).map(
+        ([k, n]) => (this.openStringNotes[k] = n.halfStep())
+      );
+      intervals--;
+    }
   }
 
   removeCapo() {
     this.chords.map((c) => c.reset());
+    this.openStringNotes = this.resetOpenStrings();
     this.capo = 0;
   }
 
@@ -49,4 +62,48 @@ export class Fretboard {
   removeChord(chord) {
     this.chords.pop(chord);
   }
+
+  resetOpenStrings() {
+    return {
+      1: new Note("E"),
+      2: new Note("A"),
+      3: new Note("D"),
+      4: new Note("G"),
+      5: new Note("B"),
+      6: new Note("E"),
+    };
+  }
+
+  getRelativeNotes(fret = 0) {
+    if (fret == 0) {
+      return this.openStringNotes;
+    }
+    var originalCapo = this.capo;
+    this.addCapo(this.capo + fret);
+    const result = { ...this.openStringNotes };
+    this.addCapo(originalCapo);
+    return result;
+  }
 }
+
+const logBoardState = (board) => {
+  console.log(" ===== Fretboard State ===== ");
+  console.log("capo", board.capo);
+  console.log(
+    "chords",
+    board.chords.map((c) => c.getName())
+  );
+  console.log(
+    "open string notes",
+    Object.values(board.openStringNotes).map((n) => n.getName())
+  );
+  console.log(
+    "relative notes",
+    Object.values(board.getRelativeNotes(1)).map((n) => n.getName())
+  );
+  console.log(" ");
+};
+
+var board = new Fretboard();
+board.addChord(allChords[0]);
+logBoardState(board);
